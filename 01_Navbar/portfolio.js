e6d7ace45c13490e1536e9154ed965914efe7e69
leesumin <leesumin90@naver.com>
2019-01-24 13:15:17 +0900

    sync

--- 01_Navbar/portfolio.js
+++ 01_Navbar/portfolio.js
@@ -29,10 +29,17 @@ function menuToggle(){
 document.getElementById('toggleBtn').addEventListener('click', menuToggle);
 
 function moveTo(id) {
-  if(id == 'home'){
+  if(id == 'brand'){
     window.scrollTo(0, 0);
   } else {
     window.scrollTo(0, document.getElementById(id).offsetTop - 70);
   }
   document.getElementById('menu').classList.remove('show');
 }
+
+document.getElementById('navbarBrand').addEventListener('click', moveTo.bind(null,'brand'));
+document.getElementById('navbarAbout').addEventListener('click', moveTo.bind(null,'about'));
+document.getElementById('navbarService').addEventListener('click', moveTo.bind(null,'service'));
+document.getElementById('navbarPortfolio').addEventListener('click', moveTo.bind(null,'portfolio'));
+document.getElementById('navbarReview').addEventListener('click', moveTo.bind(null,'review'));
+
